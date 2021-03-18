fb704a24bc47bdff1227ee7a859efabe71d48ba7
Jose Victor Persset <victor.persset@gmail.com>
2021-03-18 16:26:54 -0300

    Avançando no Aprendizado de Rotas!

--- routes/web.php
+++ routes/web.php
@@ -17,24 +17,41 @@ use Illuminate\Support\Facades\Route;
     return 'Olá, seja bem-vindo ao curso!';
 });*/
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/sobrenos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+Route::get('/sobrenos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 
 Route::get('/login', function () {
     return 'Login';
+})->name('site.login');
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    })->name('app.clientes');
+
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    })->name('app.fornecedores');
+
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    })->name('app.produtos');
 });
 
-Route::get('/clientes', function () {
-    return 'Clientes';
+Route::get('rota1', function () {
+    echo 'rota1';
+})->name('site.rota1');
+
+Route::get('rota2', function () {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+//Route::redirect('/rota2', '/rota1');
+
+Route::fallback(function () {
+   echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para a página inicial';
 });
 
-Route::get('/fornecedores', function() {
-    return 'Fornecedores';
-});
-
-Route::get('/produtos', function () {
-    return 'Produtos';
-});
