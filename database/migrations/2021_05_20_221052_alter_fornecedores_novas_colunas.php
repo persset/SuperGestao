0c73811258beffafb4057ed9a6917ad91b47b256
Jose Victor Persset <victor.persset@gmail.com>
2021-05-20 19:41:16 -0300

    Prosseguindo com migrations, migrations para produtos e fornecedores

---
+++ database/migrations/2021_05_20_221052_alter_fornecedores_novas_colunas.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterFornecedoresNovasColunas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->string('uf', 2);
+            $table->string('email', 150);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // A funcao down sempre reverte as acoes realizadas pela funcao up.
+        
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->dropColumn('uf', 'email');
+        });
+    }
+}
